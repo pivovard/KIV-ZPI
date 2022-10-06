336a3fa416629d875002a087e19918ba18decae9
pivovard <pivovar.david@gmail.com>
2022-10-06 21:10:45 +0200

    format

--- M5Stack/cv03/GetStatus.py
+++ M5Stack/cv03/GetStatus.py
@@ -13,7 +13,7 @@ lTime = M5Label('Time', x=15, y=15, color=0xFFFFFF, font=FONT_MONT_14, parent=No
 lTouch = M5Label('X: 0, Y: 0', x=15, y=50, color=0xFFFFFF, font=FONT_MONT_14, parent=None)
 switch = M5Switch(x=15, y=85, w=70, h=30, bg_c=0xCCCCCC, color=0x0288FB, parent=None)
 
-rtc.settime('ntp', host='cn.pool.ntp.org', tzone=1)
+rtc.settime('ntp', host='cn.pool.ntp.org', tzone=2)
 
 
 while(True):
@@ -21,7 +21,7 @@ while(True):
   lBat.set_text("%d%%" % bat)
   
   time = rtc.datetime()
-  lTime.set_text("%d-%d-%d %d:%d:%d" % (time[0], time[1], time[2], time[4], time[5], time[6]))
+  lTime.set_text("%d-%d-%d %d:%02d:%02d" % (time[0], time[1], time[2], time[4], time[5], time[6]))
   
   t = touch.read()
   lTouch.set_text("X: %d, Y: %d" % (t[0], t[1]))
