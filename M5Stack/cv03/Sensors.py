c584f08647fa32ae58e93799edc5fd7b9745181d
David Pivovar <david.pivovar@skodagroup.com>
2023-10-09 14:10:44 +0200

    cv03

--- M5Stack/cv03/Sensors.py
+++ M5Stack/cv03/Sensors.py
@@ -2,23 +2,25 @@ from m5stack import *
 from m5stack_ui import *
 from uiflow import *
 import unit
-
+import imu
 
 screen = M5Screen()
 screen.clean_screen()
 screen.set_screen_bg_color(0x0)
 
-sensor_type = 'TEMP'
+sensor_type = 'ENV2'
 
 if sensor_type == 'ANGLE':
   angle = unit.get(unit.ANGLE, unit.PORTA)
 if sensor_type == 'SONIC':
   ultrasonic = unit.get(unit.ULTRASONIC, unit.PORTA)
-if sensor_type == 'TEMP':
+if sensor_type == 'ENV2':
   env = unit.get(unit.ENV2, unit.PORTA)
-
-lcd.clear()
-
+if sensor_type == 'GESTURE':
+  sensor = unit.get(unit.GESTURE, unit.PORTA)
+  sensor.begin()
+  
+imu0 = imu.IMU()
 
 while not btnC.wasPressed():
   lcd.clear()
@@ -27,9 +29,14 @@ while not btnC.wasPressed():
     lcd.print(angle.read(),0,0)
   if sensor_type == 'SONIC':
     lcd.print('%f mm' % ultrasonic.distance,0,0)
-  if sensor_type == 'TEMP':
+  if sensor_type == 'ENV2':
     lcd.print('Temperature: %f' % env.temperature, 0,0)
     lcd.print('Pressure:    %f' % env.pressure, 0,15)
     lcd.print('Humidity:    %f' % env.humidity, 0,30)
+  if sensor_type == 'GESTURE':
+    lcd.print('%s' % sensor.get_gesture,0,0)
+    
+  lcd.print('Acc:  %f %f %f' % (imu0.acceleration[0], imu0.acceleration[1], imu0.acceleration[2]), 0,50)
+  lcd.print('Gyro:  %f %f %f' % (imu0.gyro[0], imu0.gyro[1], imu0.gyro[2]), 0,80)
   
   wait(0.5)
--- M5Stack/cv03/UIControls.py
+++ M5Stack/cv03/UIControls.py
@@ -2,12 +2,6 @@ from m5stack import *
 from m5stack_ui import *
 from uiflow import *
 
-
-def play():
-  power.setVibrationEnable(True)
-  speaker.playTone(131, 1)
-  power.setVibrationEnable(False)
-
 screen = M5Screen()
 screen.clean_screen()
 screen.set_screen_bg_color(0x0)
@@ -15,15 +9,29 @@ screen.set_screen_bg_color(0x0)
 btn = M5Btn("Play", x=15, y=15, w=50, h=30)
 ch = M5Checkbox("Checkbox", x=15, y=50, w=50, h=30, text_c=0x0288FB, check_c=0xCCCCCC)
 switch = M5Switch(x=15, y=85, w=70, h=30, bg_c=0xCCCCCC, color=0x0288FB)
+slider = M5Slider(15,120,100,30, 0,100)
 
 def pressed_cb():
-    play()
+    lcd.print("btn was pressed ")
+    switch.set_on()
+    a = slider.get_value()
+    slider.set_value(a+10)
+    
+    power.setVibrationEnable(True)
+    wait(2)
+    power.setVibrationEnable(False)
+    speaker.playTone(131, 1)
+    
+def swon():
+  lcd.print("Switch on")
+  
+def onChange(value):
+  lcd.print("Slider value: ")
+  lcd.print(value)
 
 btn.pressed(pressed_cb)
-
-
-play()
+switch.on(swon)
+slider.changed(onChange)
 
 while(not btnC.wasPressed()):
-  ch.set_checked(switch.get_state())
   wait(0.2)
