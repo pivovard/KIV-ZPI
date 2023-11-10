97729b8ac49544727c7e23a014bb6a7acbc770ca
David Pivovar <pivovar.david@gmail.com>
2023-11-11 00:32:30 +0100

    mqtt 2

---
+++ M5Stack/cv06_MQTT/mqtt2.py
@@ -0,0 +1,35 @@
+from m5stack import *
+from m5stack_ui import *
+from uiflow import *
+import wifiCfg
+
+# https://github.com/fizista/micropython-umqtt.simple2/
+from simple2 import MQTTClient
+
+screen = M5Screen()
+screen.clean_screen()
+screen.set_screen_bg_color(0x222222)
+
+wifiCfg.doConnect('zpikiv', '78086975')
+
+  
+def sub_cb(topic, msg, retain, dup):
+    print((topic, msg, retain, dup))
+
+client = MQTTClient("id", 'test.mosquitto.org')
+client.set_callback(sub_cb)
+client.set_last_will('zpistatus', 'offline')
+client.connect()
+c.subscribe("zpitest")
+client.publish('zpitest', 'Hello', retain=True)
+
+while True:
+    if blocking_method:
+        c.wait_msg()
+    else:
+    c.check_msg()    
+    wait(2)
+m5mqtt.disconnect()
+
+# mosquitto_sub -t "zpitest" -v -h test.mosquitto.org
+# mosquitto_pub -t "zpitest" -m "hello" -h test.mosquitto.org -r
--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 [Dokumentace microPython Github](https://github.com/m5stack/M5Stack_MicroPython)
 [Dokumentace C (Arduino)](https://docs.m5stack.com/en/quick_start/core2/arduino)
 
+## MQTT
+[ampy](https://github.com/scientifichackers/ampy) - utilita pro nahrani souboru na m5stack
+[simple2](https://github.com/fizista/micropython-umqtt.simple2/) - micropython knihovna pro MQTT (včetně retain flagu) 
+
 ## M5Stack Emulator
 https://github.com/MartinUbl/M5Stack_MicroPython_Emulator/tree/master
 
